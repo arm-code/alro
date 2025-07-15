67b82347a19dd164522cc286fdc7b897d2b896ff
Alexis Romero Mendoza <al162917@alumnos.uacj.mx>
2025-07-14 21:44:25 -0600

    feat: adding octicons/react

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,14 +1,29 @@
+import { HomeIcon } from "@primer/octicons-react"
+import Link from "next/link"
+
+const navItems = [  
+  { href: "/about", label: "About" },
+  { href: "/pricing", label: "Pricing" },
+  { href: "/contact", label: "Contact" }
+]
 
 export const Navbar = () => {
   return (
     <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
-        <span>Home</span>
+        <Link href={"/"} className="flex items-center">
+          <HomeIcon className="mr-2"/>
+          <span>Home</span>
+        </Link>
 
         <div className="flex flex-1"></div>
 
-        <a className="mr-2" href="/about">About</a>
-        <a className="mr-2" href="/pricing">Pricing</a>
-        <a className="mr-2" href="/contact">Contact</a>
+        {
+          navItems.map((item) => (
+            <Link key={item.href} href={item.href} className="px-2">
+              {item.label}    
+            </Link>
+          ))
+        }
     </nav>
   )
 }
